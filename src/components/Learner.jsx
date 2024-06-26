2de48aa9dd10aa2d9b2f4e383d92074674b4bb0e
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-06-26 13:45:53 -0700

    updated learner and score

---
+++ src/components/Learner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Learner() {
+  return (
+    <div className="classLearner">
+    Learner
+    <h1>Name</h1>
+    <h1>Bio</h1>
+    </div>
+
+  )
+}
+
+// return Renders the learners's name & bio properties.
+// return Renders a <Score> component for each score object in the learner's scores property.
---
+++ src/components/Score.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Score(props) {
+  return (
+    <div className="classScore">
+        Score
+        <h1>Date.props</h1>
+        <h1>Score.props</h1>
+    </div>
+  )
+}
